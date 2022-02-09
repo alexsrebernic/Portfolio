9d9c0d036bcf1946ad2b887d8362e933b814dd33
alexsreberni <alexsrebernic@gmail.com>
2022-02-09 15:46:55 -0300

    end Projects tab

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -54,7 +54,7 @@ export default {
                     image:"src/assets/projectsImages/facebook.png",
                     gif:"src/assets/projectsGifs/facebook.gif",
                     githubRepository:"https://github.com/alexsrebernic/Social_Media_TOP",
-                    detailsEnglish:"",
+                    detailsEnglish:"In this application you can register and sign in with the autenticacion of JWT, publish and remove posts, give them like or dislike and add comments to posts. Chat in real time with other people and edit your profile with the technology web sockets, all these interactions are received by a server created by me, i was designed with the architecture REST API. In the Github documentation there are the steps to start it locally and the stack used",
                     detailsSpanish:"En esta aplicacion se puede registrar y iniciar sesion con la autenticacion de JWT, publicar,eliminar,dar like o dislikes y añadir comentarios a posts, mandar mensajes en tiempo real y editar tu perfil con la tecnologia web sockets. Todas estas interacciones las recibe un server creado por mi que fue diseñado con la arquitectura Restful API, en la documentacion de Github estan los pasos para iniciarlo localmente y el stack utilizado.",
                     pageUrl:"https://condescending-chandrasekhar-2a90a5.netlify.app/"
                 },
@@ -65,8 +65,8 @@ export default {
                     image:"src/assets/projectsImages/coingecko.png",
                     gif:"src/assets/projectsGifs/coingecko-clone.gif",
                     githubRepository:"https://github.com/alexsrebernic/Coingecko_Clone_TOP",
-                    detailsEnglish:"",
-                    detailsSpanish:"En esta pagina se pueden ver el precio de las primeras 100 criptomonedas y sus graficos, los exchanges y se puede iniciar sesion por e-mail para guardar tus criptomonedas favoritas. Para lograr hacer este proyecto se utilizo la API de Coingecko y la libreria Chart.js, en la documentacion de Github estan los pasos para iniciarlo localmente y el stack utilizado.",
+                    detailsEnglish:"On this page you can see the price of the first 100 criptocurrency and ther charts,besides the exchanges you can log in with email to mark as favourite your favorite criptocurrency's. To achieve this project, i used the API of Coingecko and the library Chart.js,In the Github documentation there are the steps to start it locally and the stack used  ",
+                    detailsSpanish:"En esta pagina se pueden ver el precio de las primeras 100 criptomonedas y sus graficos,ademas de los exchanges,  y se puede iniciar sesion por e-mail para guardar tus criptomonedas favoritas. Para lograr hacer este proyecto se utilizo la API de Coingecko y la libreria Chart.js, en la documentacion de Github estan los pasos para iniciarlo localmente y el stack utilizado.",
                     pageUrl:"https://alexsrebernic.github.io/Coingecko_Clone_TOP/#/"
                 },
                 {
@@ -76,8 +76,8 @@ export default {
                     image:"src/assets/projectsImages/cv.png",
                     gif:"src/assets/projectsGifs/cv.gif",
                     githubRepository:"https://github.com/alexsrebernic/Cv_Creator_Top",
-                    detailsEnglish:"",
-                    detailsSpanish:"Aplicacion para crear tu curriculum, contiene apartado para tu informacion personal, educacion y experencia. Al finalizarlo podes descargarlo con pdf, este proyecto lo realize con React,Bootstrap y la libreria html2pdf.En la documentacion de Github estan los pasos para iniciarlo localmente y el stack utilizado",
+                    detailsEnglish:"Application to create your resume, contains section for your personal information, education and experience. At the end you can download in PDF, i did this project with React,Bootstrap and the library html2pdf,In the Github documentation there are the steps to start it locally and the stack used",
+                    detailsSpanish:"Aplicacion para crear tu curriculum, contiene apartado para tu informacion personal, educacion y experiencia. Al finalizarlo podes descargarlo con pdf, este proyecto lo realize con React,Bootstrap y la libreria html2pdf.En la documentacion de Github estan los pasos para iniciarlo localmente y el stack utilizado",
                     pageUrl:"https://alexsrebernic.github.io/Cv_Creator_Top/"
                 },
                  {
@@ -87,8 +87,8 @@ export default {
                     image:"src/assets/projectsImages/todo-list.png",
                     gif:"src/assets/projectsGifs/to-do.gif",
                     githubRepository:"https://github.com/alexsrebernic/TodoList_TOP",
-                    detailsEnglish:"",
-                    detailsSpanish:"Administra tus tareas y proyectos con una interactivo movimiento de tareas y creacion de ellas. Fue hecho con vanilla Javascript y se utilizo calendar.js. En la documentacion de Github estan los pasos para iniciarlo localmente y el stack utilizado.",
+                    detailsEnglish:"Manage your to-dos and projects with an interactive movement of task and creation of them. Is maded with vanilla Javascript and used the library Calendar.js,In the Github documentation there are the steps to start it locally and the stack used",
+                    detailsSpanish:"Administra tus tareas y proyectos con una interactivo movimiento de tareas y creacion de ellas. Fue hecho con vanilla Javascript y se utilizo Calendar.js. En la documentacion de Github estan los pasos para iniciarlo localmente y el stack utilizado.",
                     pageUrl:"https://alexsrebernic.github.io/TodoList_TOP/"
                 },
                  {
@@ -98,7 +98,7 @@ export default {
                     image:"src/assets/projectsImages/etch-a-sketch.png",
                     gif:"src/assets/projectsGifs/etch-a-sketch.gif",
                     githubRepository:"https://github.com/alexsrebernic/ETCH_A_SKETCH_TOP",
-                    detailsEnglish:"",
+                    detailsEnglish:"Simple web aplication in which you can draw with colors in a frame with configurable extension,when you finish your drawing you can download in png format.In the Github documentation there are the steps to start it locally and the stack used",
                     detailsSpanish:"Simple aplicacion web en la que podes dibujar con colores en un marco con extension configurable, al finalizar tu dibujo este se puede descargar en formato png. En la documentacion de Github estan los pasos para iniciarlo localmente y el stack utilizado.",
                     pageUrl:"https://alexsrebernic.github.io/ETCH_A_SKETCH_TOP/"
                 },
@@ -108,20 +108,20 @@ export default {
                     nameProjectSpanish:".members-only",
                     image:"src/assets/projectsImages/members-only.png",
                     gif:"src/assets/projectsGifs/members-only.gif",
-                    githubRepository:"https://github.com/alexsrebernic/ETCH_A_SKETCH_TOP",
-                    detailsEnglish:"",
-                    detailsSpanish:"Aplicacion web que consiste en un tablero de mensajes en el que para publicar un mensaje se debe ser miembro, se utilizo un backend con la arquitecura REST API. En la documentacion de Github estan los pasos para iniciarlo localmente y el stack utilizado.",
+                    githubRepository:"https://glacial-reef-98320.herokuapp.com/",
+                    detailsEnglish:"Web aplication  consisting in a message board where to publish a message you have to be member,a backend was used with the architecture REST API,In the Github documentation there are the steps to start it locally and the stack used",
+                    detailsSpanish:"Aplicacion web que consiste en un tablero de mensajes en el que para publicar un mensaje se debe ser miembro, se utilizo un backend con la arquitectura REST API. En la documentacion de Github estan los pasos para iniciarlo localmente y el stack utilizado.",
                     pageUrl:"https://alexsrebernic.github.io/ETCH_A_SKETCH_TOP/"
                 },
                 {
-                         id:7,
+                    id:7,
                     nameProjectEnglish:"Portfolio",
                     nameProjectSpanish:"Portafolio",
                     image:"src/assets/projectsImages/portfolio.png",
                     gif:"",
                     githubRepository:"https://github.com/alexsrebernic/Portfolio",
-                    detailsEnglish:"",
-                    detailsSpanish:"Esta misma pagina fue diseñada y hecha por mi, el diseño lo prediseñe en Figma y el utilizo Vue y Tailwind,full responsive.",
+                    detailsEnglish:"This page was made and designed by me, the design was made with Figma and it was used Vue and Tailwind",
+                    detailsSpanish:"Esta misma pagina fue diseñada y hecha por mi, el diseño fue hecho en Figma y se utilizo Vue y Tailwind.",
                 }
             ]
         }
@@ -135,9 +135,12 @@ import { Icon } from '@iconify/vue';
     img {
     image-rendering: auto;
     image-rendering: crisp-edges;
+
   background-size: contains;
     background-size: cover;
     object-fit:cover;
+         		object-position: left;
+
 
 }
     @import url('https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,400;0,700;1,400;1,700&family=Open+Sans:ital,wght@0,600;0,700;1,500;1,600&display=swap');
